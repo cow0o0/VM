b6435f64513d85c757a9a7c5237458ada66af79b
jeb <wangqiuyue2016@xgmailcom>
2019-01-15 13:16:49 +0800

    add

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # VM
 vm -- code interpreter
 
-# test
-
-
 # other vm
 
 other-vm is fork from https://github.com/codyebberson/vm
+
+# SimpleVM
+
+based on https://github.com/codyebberson/vm is easy vm
+
+# VM-Machine
+
+基于加解密的内容，采用汇编进行实现的VM
--- SimpleVM/vmtest.c
+++ SimpleVM/vmtest.c
@@ -34,16 +34,51 @@ int loop[] = {
         HALT                   // 24
 };
 
+int calc[] = {
+//.GLOBALS ;
+// N = 10
+        ICONST,10,            //0
+        GSTORE,0,               //2
+// sum = 0
+        ICONST,0x0,            //4
+        GSTORE,1,               //6
+// i = 0
+        ICONST,0x0,             //8
+        GSTORE,2,               //10
+//while i < n
+        GLOAD,2,                //14
+        GLOAD,0,                //16
+        ILT,                    //18
+        BRF,37,                   //19
+
+        GLOAD,1,                //21
+        GLOAD,2,                //23
+        IADD,                   //25
+        GSTORE,1,               //26
+
+        GLOAD,2,                //28
+        ICONST,1,                //30
+        IADD,                   //32
+        GSTORE,2,               //33
+        BR,14,                   //35
+
+        GLOAD,1,                //37
+        PRINT,                  //39
+
+        HALT                    //40
+
+};
 int main(int argc, char *argv[])
 {
-    int t1 = 0;
-    int t2 = 0;
-    vm_exec(hello, sizeof(hello), 0, 0, 1);
+//     vm_exec(hello, sizeof(hello), 0, 0, 1);
+
+    vm_exec(calc, sizeof(calc), 0, 0, 0);
 
 //     t1 = (clock() / (CLOCKS_PER_SEC / 1000));
 //     vm_exec(loop, sizeof(loop), 0, 2, 1);
 //     t2 = (clock() / (CLOCKS_PER_SEC / 1000));
 //     printf("duration = %d ms\n", (t2 - t1));
+
     return 0;
 }
 
