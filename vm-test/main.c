0e4132b2fc80465bf8d9ab3de5558c3b44219be3
jeb-ubuntu <www.wangqiuyue2016@gmail.com>
2019-01-18 22:35:37 +0800

    add

--- vm-test/main.c
+++ vm-test/main.c
@@ -3,11 +3,26 @@
 #include<stdlib.h>
 
 
-char cmpstr[] = {0x12,0x34,0x56,0x78};
+char cmpstr[6];
+int getStr(char *buffer,int maxLen){
+    char c;  // 读取到的一个字符
+    int len = 0;  // 当前输入的字符串的长度
+    // 一次读取一个字符，保存到buffer
+    // 直到遇到换行符(\n)，或者长度超过maxLen时，停止读取
+    while( (c=getchar()) != '\n' ){
+        buffer[len++]=c;  // 将读取到的字符保存到buffer
+        if(len>=maxLen){
+            break;
+        }
+    }
+    buffer[len]='\0';  // 读取结束，在末尾手动添加字符串结束标志
+    fflush(stdin);  // 刷新输入缓冲区
+    return len;
+}
 int main(int argc,char ** argv){
 
-
-    printf("hello\n");
+    getStr(cmpstr,5);
+    printf("hello %s\n",cmpstr);
     return 0;
 }
     
